9eeb7233c25a0d9cea003c435fe6155b00d42370
PratyushPal29 <29palpratyush04@gmail.com>
2025-02-15 20:32:06 +0530

    modified:   src/components/Home/HeroBg/HeroBg.tsx 	modified:   src/components/Home/HeroGlobeText/HeroGlobeText.tsx

--- src/components/Home/HeroBg/HeroBg.tsx
+++ src/components/Home/HeroBg/HeroBg.tsx
@@ -18,13 +18,13 @@ const HeroBg = () => {
 
         {/* LeftHero Decorative Element with Shadow */}
         <div className="absolute top-0 left-0 h-full flex items-center">
-          <div className="relative w-[500px] h-[680px] md:w-[600px] md:h-[750px] lg:w-[700px] lg:h-[800px] rotate-[-30deg]">
+          <div className="relative w-[650px] h-[750px] lg:w-[700px] lg:h-[800px] rotate-[-25deg] lg:rotate-[-30deg]">
             {/* Main Image */}
             <Image
               src="/assets/home/LeftHero.png"
               alt="Left Hero Decorative Element"
               fill
-              className="object-contain -translate-x-[160px] -translate-y-[60px] lg:-translate-x-[150px] lg:-translate-y-[80px]"
+              className="object-contain -translate-x-[190px] -translate-y-[160px] lg:-translate-x-[150px] lg:-translate-y-[80px]"
             />
 
             {/* Shadow Image (slightly offset) */}
@@ -32,7 +32,7 @@ const HeroBg = () => {
               src="/assets/home/LeftHero.png"
               alt="Left Hero Shadow"
               fill
-              className="object-contain opacity-30 -translate-x-[250px] -translate-y-[110px] lg:-translate-x-[320px] lg:-translate-y-[65px]"
+              className="object-contain opacity-30 -translate-x-[300px] -translate-y-[170px] lg:-translate-x-[320px] lg:-translate-y-[65px]"
             />
           </div>
         </div>
