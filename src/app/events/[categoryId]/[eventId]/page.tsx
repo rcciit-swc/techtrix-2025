f29641907e73228d68445efa36d9e28be62e8984
PratyushPal29 <29palpratyush04@gmail.com>
2025-02-19 23:34:08 +0530

    modified:   src/app/events/[categoryId]/[eventId]/page.tsx

--- src/app/events/[categoryId]/[eventId]/page.tsx
+++ src/app/events/[categoryId]/[eventId]/page.tsx
@@ -1,36 +1,55 @@
-import EventDetails from '@/components/events/EventDetails';
-import { getEventByName } from '@/utils/functions';
-import { Metadata } from 'next';
-import { notFound } from 'next/navigation';
-import { cache } from 'react';
+// import EventDetails from '@/components/events/EventDetails';
+// import { getEventByName } from '@/utils/functions';
+// import { Metadata } from 'next';
+// import { notFound } from 'next/navigation';
+// import { cache } from 'react';
 
-// Create cached version of getEventByName
-const getCachedEvent = cache(async (eventName: string) => {
-  return getEventByName(eventName);
-});
+// // Create cached version of getEventByName
+// const getCachedEvent = cache(async (eventName: string) => {
+//   return getEventByName(eventName);
+// });
 
-export async function generateMetadata({
+// export async function generateMetadata({
+//   params,
+// }: {
+//   params: { eventId: string };
+// }): Promise<Metadata> {
+//   const eventData = await getCachedEvent(decodeURIComponent(params.eventId));
+//   return {
+//     title: eventData ? `${eventData.name} - Events` : 'Event Not Found',
+//     description: eventData ? `Details for ${eventData.name}` : '',
+//   };
+// }
+
+// export default async function EventDetailsPage({
+//   params,
+// }: {
+//   params: { eventId: string };
+// }) {
+//   const eventData = await getCachedEvent(decodeURIComponent(params.eventId));
+
+//   if (!eventData) {
+//     notFound();
+//   }
+
+//   return <EventDetails eventData={eventData} />;
+// }
+
+import React from "react";
+import EventLayout from "../../layout";
+import EventDetails from "@/components/Event/EventDetails";
+
+const Events = ({
   params,
 }: {
-  params: { eventId: string };
-}): Promise<Metadata> {
-  const eventData = await getCachedEvent(decodeURIComponent(params.eventId));
-  return {
-    title: eventData ? `${eventData.name} - Events` : 'Event Not Found',
-    description: eventData ? `Details for ${eventData.name}` : '',
-  };
-}
+  params: { categoryId: string };
+}) => {
 
-export default async function EventDetailsPage({
-  params,
-}: {
-  params: { eventId: string };
-}) {
-  const eventData = await getCachedEvent(decodeURIComponent(params.eventId));
+  return (
+    <EventLayout categoryId={params.categoryId}>
+      <EventDetails categoryId={params.categoryId} />
+    </EventLayout>
+  );
+};
 
-  if (!eventData) {
-    notFound();
-  }
-
-  return <EventDetails eventData={eventData} />;
-}
+export default Events;
