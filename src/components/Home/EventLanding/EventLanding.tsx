6a641787e11ec4e1c529a7879e86953fec54aeb9
PratyushPal29 <29palpratyush04@gmail.com>
2025-02-20 18:16:36 +0530

    modified:   src/components/Home/EventLanding/EventLanding.tsx

--- src/components/Home/EventLanding/EventLanding.tsx
+++ src/components/Home/EventLanding/EventLanding.tsx
@@ -1,15 +1,26 @@
 'use client';
 import { LayoutGrid } from '@/components/ui/layout-grid';
+import Image from 'next/image';
 
 export default function EventLanding() {
   return (
     <div className="w-full flex flex-col items-center justify-center bg-black">
-      <h1 className="font-kagitingan text-5xl text-white">EVENTS</h1>
+      <div className="inset-0">
+        <Image
+          src="/assets/home/star2.svg"
+          alt="Starfield Background"
+          fill
+          className="object-cover"
+          quality={100}
+        />
+        {/* <div className="absolute bottom-0 w-full h-10 bg-[#030204] bg-opacity-60 backdrop-blur-sm" /> */}
+      </div>
+      <h1 className="font-kagitingan text-5xl text-white z-10">EVENTS</h1>
       <div className="w-full max-w-7xl">
         <LayoutGrid cards={cards} />
       </div>
     </div>
-  )
+  );
 }
 
 const Skeleton = () => {
