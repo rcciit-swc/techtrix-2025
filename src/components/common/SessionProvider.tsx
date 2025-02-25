21604145fb73ec49adeabf563057a38a73ce2cc1
Soumyaraj Bag <soumyarajbag@gmail.com>
2025-02-26 02:56:16 +0530

    fix:deployment

--- src/components/common/SessionProvider.tsx
+++ src/components/common/SessionProvider.tsx
@@ -2,22 +2,16 @@
 import { useEvents } from '@/lib/stores';
 import { useUser } from '@/lib/stores/user';
 import { supabase } from '@/utils/functions/supabase-client';
-import { useParams } from 'next/navigation';
 import { useEffect } from 'react';
 
 const SessionProvider = () => {
-  const params = useParams();
-  console.log(params);
   const setUser = useUser((state) => state.setUserData);
   const setEvents = useEvents((state) => state.setEventsData);
   useEffect(() => {
     const readUserSession = async () => {
       const { data } = await supabase.auth.getSession();
-      typeof window !== 'undefined' && localStorage.setItem('ref', params?.ref);
-      if (data?.session?.user) {
+      if (data?.session?.user.user_metadata?.avatar_url) {
         setUser();
-      } else {
-        typeof window !== 'undefined' && localStorage.setItem('ref', params?.ref);
       }
     };
     readUserSession();
